3cf42c794c69de15cd7db41ee0837a085b350552
aman shrestha <aman.shrestha@sysqube.com.np>
2024-10-17 12:45:21 +0545

    removed unnecessary fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:keyboard_dismisser/keyboard_dismisser.dart';
+import 'package:textformfield_filtering_data/data.dart';
 
 import 'textformfield_reusable.dart';
 
@@ -15,149 +16,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: TextFormFieldFilteringData()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: TextFormFieldFilteringData());
   }
 }
 
 class TextFormFieldFilteringData extends StatelessWidget {
-  var country = ["Nepal"];
-  var locationData = [
-    {
-    "name": "Bagmati",
-    "city": [
-      {
-        "city_name": "Kathmandu",
-        "zones": ["Kuleshwor", "New Baneshwor", "Boudha"],
-      },
-      {
-        "city_name": "Lalitpur",
-        "zones": ["Patan", "Jawalakhel", "Lagankhel"],
-      },
-      {
-        "city_name": "Bhaktapur",
-        "zones": ["Suryabinayak", "Changu Narayan", "Dattatreya"],
-      },
-    ],
-  },
-  {
-    "name": "Gandaki",
-    "city": [
-      {
-        "city_name": "Pokhara",
-        "zones": ["Lakeside", "Mahendrapool", "Nayabazar"],
-      },
-      {
-        "city_name": "Gorkha",
-        "zones": ["Gorkha Bazaar", "Barpak", "Manakamana"],
-      },
-      {
-        "city_name": "Lamjung",
-        "zones": ["Besisahar", "Sundarbazar", "Bhujung"],
-      },
-    ],
-  },
-  {
-    "name": "Lumbini",
-    "city": [
-      {
-        "city_name": "Butwal",
-        "zones": ["Devinagar", "Kalikanagar", "Traffic Chowk"],
-      },
-      {
-        "city_name": "Kapilvastu",
-        "zones": ["Taulihawa", "Krishnanagar", "Buddhanagar"],
-      },
-      {
-        "city_name": "Palpa",
-        "zones": ["Tansen", "Srinagar", "Madibas"],
-      },
-    ],
-  },
-  {
-    "name": "Karnali",
-    "city": [
-      {
-        "city_name": "Surkhet",
-        "zones": ["Birendranagar", "Itram", "Latikoili"],
-      },
-      {
-        "city_name": "Jumla",
-        "zones": ["Khalanga", "Tatopani", "Chandannath"],
-      },
-      {
-        "city_name": "Dailekh",
-        "zones": ["Narayan", "Dullu", "Bhagwatimai"],
-      },
-    ],
-  },
-  {
-    "name": "Koshi",
-    "city": [
-      {
-        "city_name": "Biratnagar",
-        "zones": ["Rani", "Buddhanagar", "Mills Area"],
-      },
-      {
-        "city_name": "Dharan",
-        "zones": ["Bhanuchowk", "Saptarangi Park", "Pindeshwori"],
-      },
-      {
-        "city_name": "Itahari",
-        "zones": ["Itahari Chowk", "Hanuman Mandir", "Tarahara"],
-      },
-    ],
-  },
-  {
-    "name": "Madhesh",
-    "city": [
-      {
-        "city_name": "Janakpur",
-        "zones": ["Janaki Mandir", "Ramanand Chowk", "Bhanu Chowk"],
-      },
-      {
-        "city_name": "Birgunj",
-        "zones": ["Ghantaghar", "Adarsh Nagar", "Powerhouse"],
-      },
-      {
-        "city_name": "Rajbiraj",
-        "zones": ["Rajdevi Mandir", "Sakhda", "Kataiya"],
-      },
-    ],
-  },
-  {
-    "name": "Sudurpashchim",
-    "city": [
-      {
-        "city_name": "Dhangadhi",
-        "zones": ["Hasuliya", "Campus Road", "Attariya"],
-      },
-      {
-        "city_name": "Mahendranagar",
-        "zones": ["Bhasi", "Shivnagar", "Mahakali Highway"],
-      },
-      {
-        "city_name": "Dadeldhura",
-        "zones": ["Amargadhi", "Ugratara", "Gaira"],
-      },
-    ],
-  },
-  ];
   final zoneController = TextEditingController();
-  final fullNameController = TextEditingController();
+
   final addressController = TextEditingController();
   final cityController = TextEditingController();
   final stateController = TextEditingController();
-  final zipCodeController = TextEditingController();
-  final countryController = TextEditingController();
-  final phoneController = TextEditingController();
-   TextFormFieldFilteringData({super.key});
+
+  TextFormFieldFilteringData({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -177,63 +52,6 @@ class TextFormFieldFilteringData extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Text(
-                  "Full Name",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormFieldReusable(
-                  hint: "Entern Your Full Name",
-                  textEditingController: fullNameController,
-                ),
-                const SizedBox(height: 30),
-                const Text(
-                  "Country",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  inputFormatters: [
-                    FilteringTextInputFormatter.deny(
-                        RegExp('.*')), // This denies all input
-                  ],
-                  controller: countryController,
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Country",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color(0xffAEAEAE),
-                      ),
-                    ),
-                    suffixIcon: PopupMenuButton<String>(
-                      icon: const Icon(Icons.arrow_drop_down),
-                      onSelected: (String value) {
-                        countryController.text = value;
-                      },
-                      itemBuilder: (BuildContext context) {
-                        return country
-                            .map<PopupMenuItem<String>>((String value) {
-                          return PopupMenuItem(
-                              value: value,
-                              child: Text(value));
-                        }).toList();
-                      },
-                    ),
-                  ),
-                ),
-                // TextFormFieldReusable(
-                //   hint: "Entern Your Country",
-                //   textEditingController: countryController,
-                // ),
-                const SizedBox(height: 30),
-                const Text(
                   "State/Province",
                   style: TextStyle(
                     fontSize: 14,
@@ -241,7 +59,6 @@ class TextFormFieldFilteringData extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 10),
-
                 TextField(
                   inputFormatters: [
                     FilteringTextInputFormatter.deny(
@@ -278,7 +95,6 @@ class TextFormFieldFilteringData extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 30),
-
                 const Text(
                   "City",
                   style: TextStyle(
@@ -409,38 +225,11 @@ class TextFormFieldFilteringData extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(height: 30),
-                const Text(
-                  "Zip Code",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormFieldReusable(
-                  hint: "Entern Your Zip Code",
-                  textEditingController: zipCodeController,
-                ),
-                const SizedBox(height: 30),
-                const Text(
-                  "Phone Number",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormFieldReusable(
-                  hint: "Entern Your Phone Number",
-                  textEditingController: phoneController,
-                ),
-                const SizedBox(height: 30),
-                
               ],
             ),
           ),
         ),
       ),
     );
-  }}
+  }
+}
